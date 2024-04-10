caa8dbac4f5a59b8738342ec16cc333bc7711dd0
Gabriel Chiarelli <166543791+gabrielchiarelligomes@users.noreply.github.com>
2024-04-10 16:57:15 -0300

    Create 7 Animal.java

---
+++ 7 Animal.java
@@ -0,0 +1,31 @@
+
+public class Animal {
+    private String nome
+    private String tipo
+    private String somAnimal
+    
+    public Animal (String n, String t) {
+        this.nome = n
+        this.tipo = t
+    }
+    
+    public String getNome () {
+        return nome;
+    }
+    
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+    
+     public String getTipo () {
+        return Tipo;
+    }
+    
+    public void setTipo (String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public String emitirSom ( String som) {
+        return som;
+    }
+}
