28e6699f1a79fe698a46346d87398b10207f778c
Gabriel Chiarelli <166543791+gabrielchiarelligomes@users.noreply.github.com>
2024-04-10 18:09:48 -0300

    20 Empresa.java

---
+++ 20 Empresa.java
@@ -0,0 +1,25 @@
+public class Empresa {
+	private String nome;
+	private String cnpj;
+	
+	public Empresa(String n, String c) {
+		this.nome = n;
+		this.cnpj = c;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+}
