6c1456a188b0a19501e06a46faac8e5a7766afc3
Gabriel Chiarelli <166543791+gabrielchiarelligomes@users.noreply.github.com>
2024-04-10 17:14:13 -0300

    11 Gato.java

---
+++ 11 Gato.java
@@ -0,0 +1,38 @@
+public class Gato {
+	private String nome;
+	private String raca;
+	private int idade;
+	
+	public Gato (String n, String r, int i) {
+		this.nome = n;
+		this.raca = r;
+		this.idade = i;
+}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}	
+	
+	public String toString() {
+			return "miau";
+	}}
