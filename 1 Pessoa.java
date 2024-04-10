2190d18857af156d943e5d6053ee5af90b881d83
Gabriel Chiarelli <166543791+gabrielchiarelligomes@users.noreply.github.com>
2024-04-10 16:39:05 -0300

    Create 1 Pessoa.java

---
+++ 1 Pessoa.java
@@ -0,0 +1,33 @@
+package teste;
+public class Pessoa {
+	private String nome;
+	private int idade;
+	private double altura;
+	public Pessoa(String n, int i, double a) {
+		this.nome = n;
+		this.idade = i;
+		this.altura = a;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public String toString() {
+		return "nome: " + getNome() + "\nidade: " + getIdade() + "\naltura: "  + getAltura() ;
+	}
+}
